c23be33ea4d67f2cdd57dbc371062f044c48801b
beaverUTSA <biever.brian@gmail.com>
2020-12-04 08:45:39 -0600

    Add files via upload

---
+++ ddiQuestion1.py
@@ -0,0 +1,8 @@
+#Question 1:
+#Given a table that holds websites (WEBSITE_ID, Name, URL),
+#website pages (PAGE_ID, WEBSITE_ID, path), and website 
+#vulnerabilities (VULN_ID, PAGE_ID, data), find all of the vulnerabilities 
+#on website foo.com that pertain to the login page login.html. 
+#Your SQL should be performant for larger data sets.
+
+#SQL answer
