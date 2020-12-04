a59f6d00d02acb3f898815a1b879092c77a9a7b5
beaverUTSA <biever.brian@gmail.com>
2020-12-04 09:06:02 -0600

    Update ddiQuestion1.py

--- ddiQuestion1.py
+++ ddiQuestion1.py
@@ -6,3 +6,45 @@
 #Your SQL should be performant for larger data sets.
 
 #SQL answer
+#SELECT VULN_ID, PAGE_ID, data
+#    FROM vulnerabilities
+#    WHERE PAGE_ID IN (
+#    	SELECT PAGE_ID 
+#	FROM pages 
+#        WHERE WEBSITE_ID IN (
+#		SELECT WEBSITE_ID 
+#       		FROM websites 
+#		WHERE Name = "foo") 
+#	AND path LIKE '%login.html%');
+
+import mysql.connector
+import sys
+
+#dont store passwords in scripts
+server = mysql.connector.connect(
+    host="",
+    user="",
+    password=""
+    database=""
+)
+
+cursor = server.cursor()
+
+#TODO scrub inputs to stop SQL injection
+site = argv[1]
+page = argv[2]
+
+query = ('SELECT VULN_ID, PAGE_ID, data'
+         'FROM vulnerabilities'
+         'WHERE PAGE_ID IN ('
+         '   SELECT PAGE_ID '
+         '    FROM pages '
+         '    WHERE WEBSITE_ID IN ('
+         '        SELECT WEBSITE_ID '
+         '        FROM websites '
+         '        WHERE Name = "' + site + '")' 
+         '    AND path LIKE "%' + page + '%");')
+
+cursor.excecute(query)
+
+return cursor.fetchall()
