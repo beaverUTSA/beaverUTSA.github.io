96c2067e935ac97745f28bb94bc022680418859d
beaverUTSA <biever.brian@gmail.com>
2020-12-04 12:05:01 -0600

    Update ddiQuestion2.py

--- ddiQuestion2.py
+++ ddiQuestion2.py
@@ -6,6 +6,14 @@ def apply_port_exclusions(include_ports, exclude_ports):
     #print("include")
     ret_ports = []
     exclude_found = False
+    x = 0
+    while x < len(include_ports):
+        if x > 0 and include_ports[x-1][1]+1 == include_ports[x][0]:
+            include_ports[x][0] = include_ports[x-1][0]
+            include_ports.pop(x-1)
+            x-=1
+        #print(include_ports[x])
+        x += 1
     for x in include_ports:
         for y in exclude_ports:  
             if(x[0] < y[0] and x[1] > y[1]):
@@ -27,6 +35,7 @@ print(apply_port_exclusions([[8000, 9000], [80, 80], [22, 23]],[[1024, 1024], [8
 print("[[22, 23], [80, 80], [8000, 8079], [8081, 9000]] expected")
 print(apply_port_exclusions([[1,65535]],[[1000,2000], [500, 2500]]))
 print("[[1, 499], [2501, 65535]] expected")
+#all thats missing here is joining included ports and removing the ones out of range
 print(apply_port_exclusions([[1,1], [3, 65535], [2, 2]],[[1000, 2000], [500, 2500]]))
 print("[[1, 499], [2501, 65535]] expected")
 print(apply_port_exclusions([],[[8080, 8080]]))
